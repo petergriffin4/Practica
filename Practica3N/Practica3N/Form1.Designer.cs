5aa0c6744a2d1f613053d44f88b2d6a19c1da694
petergriffin4 <feacuna@uamv.edu.ni>
2024-11-04 09:42:50 -0600

    Add files via upload

---
+++ Practica3N/Practica3N/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Practica3N
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblPsw = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPsw = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(92, 94);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(43, 13);
+            this.lblUser.TabIndex = 0;
+            this.lblUser.Text = "Usuario";
+            // 
+            // lblPsw
+            // 
+            this.lblPsw.AutoSize = true;
+            this.lblPsw.Location = new System.Drawing.Point(92, 144);
+            this.lblPsw.Name = "lblPsw";
+            this.lblPsw.Size = new System.Drawing.Size(61, 13);
+            this.lblPsw.TabIndex = 1;
+            this.lblPsw.Text = "Contraseña";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(173, 94);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(202, 20);
+            this.txtUser.TabIndex = 2;
+            // 
+            // txtPsw
+            // 
+            this.txtPsw.Location = new System.Drawing.Point(173, 144);
+            this.txtPsw.Name = "txtPsw";
+            this.txtPsw.Size = new System.Drawing.Size(202, 20);
+            this.txtPsw.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(156, 206);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(124, 42);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Iniciar Sesion";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPsw);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.lblPsw);
+            this.Controls.Add(this.lblUser);
+            this.Name = "Form1";
+            this.Text = "LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label lblPsw;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPsw;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ Practica3N/Practica3N/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica3N
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string contraseña = txtPsw.Text.Trim();
+            string usuario = txtUser.Text.Trim();
+            if (string.IsNullOrEmpty(usuario) )
+            {
+                MessageBox.Show("Por favor, Ingresa el usuario.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtUser.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(contraseña))
+            {
+                MessageBox.Show("Por favor, Ingresa la contraseña.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtPsw.Focus();
+                return;
+            }
+        }
+    }
+}
